27a4593fbb87485181f3e00d396125a0f41ebd91
tenmonster <tenmonster@hotmail.com>
2025-02-20 14:51:56 +0700

    Setup Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        colorNight: 'var(--color-night)',
+        colorEerie: 'var(--color-eerie-black)',
+        colorMalachite: 'var(--color-malachite)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
